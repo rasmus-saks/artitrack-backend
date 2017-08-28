610a44a26ee26f70b79b31c2f92bd2ff4e49fe77
Rasmus Saks <rasmussaks1@gmail.com>
2017-08-28 17:11:40 +0300

    Add invalid steam64 and cookie auth tests

--- src/test/java/me/artitrack/backend/BaseIntegrationTest.java
+++ src/test/java/me/artitrack/backend/BaseIntegrationTest.java
@@ -1,10 +1,54 @@
 package me.artitrack.backend;
 
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import me.artitrack.backend.model.User;
+import me.artitrack.backend.repository.UserRepository;
+import me.artitrack.backend.security.JwtTokenUtil;
+import me.artitrack.backend.security.JwtUser;
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.TestPropertySource;
+
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {"spring.jpa.hibernate.ddl-auto=create-drop"})
 public abstract class BaseIntegrationTest extends BaseTest {
+  protected static final String TEST_USER_STEAM64 = "1234567";
 
+  @Value("${local.server.port}")
+  private int port;
+
+  @Autowired
+  private JwtTokenUtil tokenUtil;
+
+  @Autowired
+  private UserRepository userRepository;
+  private User user = new User(TEST_USER_STEAM64);
+
+  @Before
+  public void setUp() throws Exception {
+    RestAssured.port = port;
+    user = userRepository.save(user);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    userRepository.delete(user);
+  }
+
+  public RequestSpecification authed() {
+    String token = getToken();
+    return given().header("Authorization", "Bearer " + token);
+  }
+
+  public String getToken() {
+    UserDetails details = new JwtUser(user);
+    return tokenUtil.generateToken(details);
+  }
 }
--- src/test/java/me/artitrack/backend/controller/ApiControllerTest.java
+++ src/test/java/me/artitrack/backend/controller/ApiControllerTest.java
@@ -1,35 +1,14 @@
 package me.artitrack.backend.controller;
 
-import io.restassured.RestAssured;
 import me.artitrack.backend.BaseIntegrationTest;
-import me.artitrack.backend.model.User;
-import me.artitrack.backend.repository.UserRepository;
-import me.artitrack.backend.security.JwtTokenUtil;
-import me.artitrack.backend.security.JwtUser;
 import org.apache.http.HttpStatus;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class ApiControllerTest extends BaseIntegrationTest {
-  private static final String TEST_USER_STEAM64 = "1234567";
-
-  @Value("${local.server.port}")
-  private int port;
-
-  @Autowired
-  private JwtTokenUtil tokenUtil;
-
-  @Autowired
-  private UserRepository userRepository;
-  private User user = new User(TEST_USER_STEAM64);
 
   @Test
   public void testUnauthorized() throws Exception {
@@ -38,27 +17,30 @@ public class ApiControllerTest extends BaseIntegrationTest {
         .statusCode(HttpStatus.SC_UNAUTHORIZED);
   }
 
-
   @Test
   public void testAuthorized() throws Exception {
-    UserDetails details = new JwtUser(user);
-    String token = tokenUtil.generateToken(details);
-    given()
-        .header("Authorization", "Bearer " + token)
+    authed()
         .get("/api/steam64/" + TEST_USER_STEAM64)
         .then()
         .statusCode(HttpStatus.SC_OK)
         .body("steam64", is(TEST_USER_STEAM64));
   }
 
-  @Before
-  public void setUp() throws Exception {
-    RestAssured.port = port;
-    user = userRepository.save(user);
+  @Test
+  public void testInvalidSteam64() throws Exception {
+    authed()
+        .get("/api/steam64/1123")
+        .then()
+        .statusCode(HttpStatus.SC_NOT_FOUND);
   }
 
-  @After
-  public void tearDown() throws Exception {
-    userRepository.delete(user);
+  @Test
+  public void testCookieAuthentication() throws Exception {
+    given()
+        .cookie("auth-token", getToken()).
+        get("/api/steam64/" + TEST_USER_STEAM64)
+        .then()
+        .statusCode(HttpStatus.SC_OK)
+        .body("steam64", is(TEST_USER_STEAM64));
   }
 }
